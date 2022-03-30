0063b81b418a171f9e7401ef23e03744e866d947
Aqqori <101850988+Aqqori@users.noreply.github.com>
2022-03-30 07:56:52 +0500

    Add files via upload

---
+++ githubchik.py
@@ -0,0 +1,13 @@
+#mehmon = ["ali", "vali", "gani", "asad", "botu"]
+#for mehri in mehmon: 
+#        print( "salom", mehri)
+
+#sonlar = list(range(1,11))
+#for son in sonlar:
+ #   print(f"{son} ning kvadrati {son**2} ga teng")
+    
+dustlar = []
+print("3 ta dust nomini yozing:")
+for j in range(1,4):
+    dustlar.append(input(f"{j} - dustingizni ismini yozing: "))
+print(dustlar)
